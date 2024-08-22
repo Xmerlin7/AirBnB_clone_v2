9d2e147ffe6ad4ec8d4121082199ad1f83e78149
Seif Allah <saifeldarageely@gmail.com>
2024-08-22 19:12:53 +0300

    #

--- models/place.py
+++ models/place.py
@@ -1,71 +1,85 @@
 #!/usr/bin/python3
-"""Place subclass that inherits from BaseModel"""
-from os import getenv
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+"""
+    module containing places to represent the place
+    module containing places to represent the place
+"""
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 import models
-import shlex
-# from models.amenity import Amenity
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if storage_engine == "db":
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
-    """Place class to represent new places"""
+    """
+        Place class to represent places
+        Place class to represent places
+    """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review",
-                               cascade='all, delete, delete-orphan',
-                               backref="place")
+    if (storage_engine == "db"):
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
+        reviews = relationship("Review", back_populates="place")
         amenities = relationship("Amenity",
                                  secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates="place_amenities")
+                                 back_populates="place_amenities",
+                                 viewonly=False)
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
-            """Getter method for the Review class"""
-            reviews = models.storage.all()
-            reviews_list = []
+            """getter function for reviews attribute"""
             result = []
-            for key in reviews:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    reviews_list.append(reviews[key])
-            for el in reviews_list:
-                if (el.place_id == self.id):
-                    result.append(el)
+            temp = models.dummy_classes['Review']
+            for r in models.storage.all(temp).values():
+                if r.place_id == self.id:
+                    result.append(r)
             return result
 
         @property
         def amenities(self):
-            """Getter method for the Amenity class"""
-            return self.amenity_ids
+            """getter function for amenity attribute"""
+            result = []
+            temp = models.dummy_classes['Amenity']
+            for a in models.storage.all(temp).values():
+                if a in self.amenity_ids:
+                    result.append(a)
+            return result
 
         @amenities.setter
-        def amenities(self, obj=None):
-            """Setter method for the Amenity class"""
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+        def amenities(self, obj):
+            """ setter for amenities class """
+            temp = models.dummy_classes['Amenity']
+            if (isinstance(obj, models.storage.all(temp))):
                 self.amenity_ids.append(obj.id)
